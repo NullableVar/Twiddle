a4317ba78b9b677c27950872c4ba043591f0eab6
NullableVar <aarionit.tahiraj@gmail.com>
2025-09-18 10:59:04 +0200

    Token Provider

---
+++ Twiddle.API/Core/ITokenProvider.cs
@@ -0,0 +1,8 @@
+﻿using Twiddle.Models;
+
+namespace Twiddle.API.Core;
+
+public interface ITokenProvider
+{
+    public string Create(UserModel user);
+}
---
+++ Twiddle.API/Core/TokenProvider.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using Twiddle.Models;
+
+namespace Twiddle.API.Core;
+
+internal class TokenProvider(IConfiguration configuration) : ITokenProvider
+{
+    public string Create(UserModel user)
+    {
+        string secretKey = configuration["Jwt:Secret"]!;
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        var tokenDescriptor = new SecurityTokenDescriptor()
+        {
+            Subject = new ClaimsIdentity(
+            [
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email)
+            ]),
+            Expires = DateTime.UtcNow.AddMinutes(configuration.GetValue<int>("Jwt:ExpirationInMinutes")),
+            SigningCredentials = credentials,
+            Issuer = configuration["Jwt:Issuer"],
+            Audience = configuration["Jwt:Audience"]
+        };
+
+        var handler = new JsonWebTokenHandler();
+
+        return handler.CreateToken(tokenDescriptor);
+    }
+}
