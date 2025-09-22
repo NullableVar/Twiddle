ddc3c3a3e907aeca6c93a0d068a4bf26fc99330a
NullableVar <aarionit.tahiraj@gmail.com>
2025-09-22 19:57:53 +0200

    Created TwidViewModel and refactored LoginPage and RegisterPage

---
+++ Twiddle/Components/ViewModels/TwidViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Twiddle.Components.ViewModels;
+
+public class TwidViewModel
+{
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "UserId is required")]
+    public Guid UserId { get; set; }
+
+    [Required(ErrorMessage = "Text is required")]
+    [MaxLength(1024, ErrorMessage = "Text cannot exceed 1024 characters")]
+    public string Text { get; set; } = string.Empty;
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
